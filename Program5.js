ea12fb97c22de8ad791e8b75ff423bac2225cf91
Mangesh Jadhav <mangeshjadhavmrj@gmail.com>
2023-12-06 21:25:06 +0530

    Java Script Practice

---
+++ Program5.js
@@ -0,0 +1,13 @@
+
+//remove negative numbers in array
+
+var arr = [1,3,-4,5,-6,7,7,-8,9,-6,54,-65];
+
+arr.forEach(function(val,index){
+
+  if(val < 0){
+    arr.splice(index,1);
+  }
+})
+
+console.log(arr);
