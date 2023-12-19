153ca4b476aba5c7a8a468584f61a9a25bdb0ef9
Mangesh Jadhav <mangeshjadhavmrj@gmail.com>
2023-12-19 20:42:56 +0530

    Node jsloop Practice

---
+++ ProgramH1.js
@@ -0,0 +1,14 @@
+let firstName = "raman sharma";
+let age = "32";
+let Marriage = true;
+
+
+console.log("Name of the Person is " + firstName + " and his age is " + age);
+
+if(Marriage == false) {
+    console.log(firstName + "is married");
+}
+else 
+{
+    console.log(firstName + "is not married");
+}
