c4197c11726e2a335eac0da47a8c3134b981f400
Mangesh Jadhav <mangeshjadhavmrj@gmail.com>
2024-01-02 19:15:16 +0530

    js practice

--- holopattern.js
+++ holopattern.js
@@ -1,26 +1,69 @@
+
+/*
+     *  *  *  *  *  *
+     *              * 
+     *              *
+     *              *  
+     *  *  *  *  *  *     
+
+*/
+
+
+
+
+
+function hologram(row,col){
 let string = "";
-
-for( var row = 0; row < 5; row++) {
-    for(var col = 0 ; col < 5 ; col++) {
-        if(row == 0 || row == 4) {
-            string += "* ";
+for(let i = 1;i <= row;i++){
+    for(let j = 1; j <= col; j++){
+       if(i == 1 || i == row){
+        string += "* "
+       }
+       else{
+        if(j == 1 || j == col){
+           string += "* " 
         }
-        else {
-            if(col == 0 || col == 4)
-            {
-                string += "* ";
-            }
-            else 
-            {
-                string += "  ";
-            } 
+        else{
+            string += "  "
         }
+       }
+     
     }
- string += "\n";
-
+    string += "\n";  
+}
+ return string;
 }
 
 
+let pattern = hologram(5,6)
+console.log(pattern);
 
 
-console.log(string);
+
+
+// let string = "";
+
+// for( var row = 0; row < 5; row++) {
+//     for(var col = 0 ; col < 5 ; col++) {
+//         if(row == 0 || row == 4) {
+//             string += "* ";
+//         }
+//         else {
+//             if(col == 0 || col == 4)
+//             {
+//                 string += "* ";
+//             }
+//             else 
+//             {
+//                 string += "  ";
+//             } 
+//         }
+//     }
+//  string += "\n";
+
+// }
+
+
+
+
+// console.log(string);
--- patternprint.js
+++ patternprint.js
@@ -1,12 +1,34 @@
 
-let string = "";
 
-for(var i = 0; i < 4 ; i++) {
-    for(var j = 0 ; j < 4; j++)
-    {
-        string += "* ";
-    }
-     string += "\n";
-}
+function patternPrint(row ,col){
+ 
+ let string = "";
+ 
+ for(let i = 0; i < row; i++){
+ for(let j = 0 ;j < col;j++){
+    string += "* "
+ }
+    string += '\n'; 
+ }
 
-console.log(string);
+return string;
+} 
+
+
+
+let str = patternPrint(3,5);
+console.log(str);
+
+
+
+// let string = "";
+
+// for(var i = 0; i < 4 ; i++) {
+//     for(var j = 0 ; j < 4; j++)
+//     {
+//         string += "* ";
+//     }
+//      string += "\n";
+// }
+
+// console.log(string);
