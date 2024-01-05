382d174fc72021e169e492c6b2a5900dad7a3aec
Mangesh Jadhav <mangeshjadhavmrj@gmail.com>
2024-01-05 19:14:18 +0530

    js practice

--- AsyncFunction/anyncFun2.js
+++ AsyncFunction/anyncFun2.js
@@ -1,9 +1,9 @@
 
 
-const fs = require("fs");    //its a node js library its provide to some functionality read from files and write in files  
+const fs = require("fs");    //?its a node js library its provide to some functionality read from files and write in files  
 
 fs.readFile("AsyncFunction/mj.txt","utf-8",function(err,data){ //its delegate the task to this fun
      console.log(data); //this fun is waited for  completed the  below tasks.
 })
 
-console.log("read the below"); // this runs first   
+console.log("read the below"); // todo: this runs first   
