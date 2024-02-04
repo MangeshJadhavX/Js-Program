c96a2411c4b8c7e2155c09f7e19adc6614dd2112
lokeshyc01 <100062082+lokeshyc01@users.noreply.github.com>
2024-02-04 18:17:50 +0530

    proposed changesh by lokesh chaudhari

--- middleWare/middleware.js
+++ middleWare/middleware.js
@@ -13,7 +13,14 @@ function calculateRequest (res,res,next){
 }
 
 app.get("/",calculateRequest,function(req,res){
+const opeartor = req.body.operator;
+    if(operator == *)
+       {
+           let a = Number.ParseInt(req.body.num1);
+           let b = Number.ParseInt(req.body.num2);
 
+           console.log(a * b);
+       }
 })
 
 app.post("/",calculateRequest,function(req,res){
